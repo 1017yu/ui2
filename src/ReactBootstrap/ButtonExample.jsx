b7f5a5c15b6f2dbf3224478c390f170db53c4f3c
Kled <1017yu@daum.net>
2021-12-28 19:04:21 +0900

    test

---
+++ src/ReactBootstrap/ButtonExample.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ButtonExample = () => {
+  return (
+    <div>
+      <ButtonExample />
+    </div>
+  );
+}
+
+export default ButtonExample;
---
+++ src/ReactBootstrap/ReactBootstrapExample.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ButtonExample from './ButtonExample';
+import AlertDismissible from './AlertDismissible';
+
+export default function ReactBootstrapExample(){
+  return (
+    <div>
+      <AlertDismissible />
+      <ButtonExample />
+    </div>
+  );
+}
